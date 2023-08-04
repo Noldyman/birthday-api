85ed5e234015252bd082843d0b8b85cec1baf5fc
Noud Verstijnen <noud.verstijnen@gmail.com>
2023-08-04 23:23:44 +0200

    create endpoints for birthdays today and this week

--- Controllers/BirthdayController.cs
+++ Controllers/BirthdayController.cs
@@ -10,32 +10,8 @@ namespace Birthday_Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BirthdayController : ControllerBase
+    public class BirthdaysController : ControllerBase
     {
-        [HttpGet]
-        public async Task<ActionResult<List<Birthday>>> GetAllBirthdays()
-        {
-            using (var db = new BirthdayContext())
-            {
-                var birthdays = db.Birthdays.ToList();
-                return birthdays;
-            }
-        }
-
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Birthday>> GetBirthday(int id)
-        {
-            using (var db = new BirthdayContext())
-            {
-                var birthday = await db.Birthdays.FindAsync(id);
-                if (birthday == null)
-                {
-                    return NotFound();
-                }
-                return birthday;
-            }
-        }
-
         [HttpPost]
         public async Task<ActionResult<Birthday>> CreateBirthday(Birthday newBirthday)
         {
@@ -72,6 +48,53 @@ namespace Birthday_Api.Controllers
                 db.Remove(birthday);
                 await db.SaveChangesAsync();
                 return birthday;
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Birthday>>> GetAllBirthdays()
+        {
+            using (var db = new BirthdayContext())
+            {
+                var birthdays = db.Birthdays.ToList();
+                return birthdays;
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Birthday>> GetBirthday(int id)
+        {
+            using (var db = new BirthdayContext())
+            {
+                var birthday = await db.Birthdays.FindAsync(id);
+                if (birthday == null)
+                {
+                    return NotFound();
+                }
+                return birthday;
+            }
+        }
+
+        [HttpGet("Today")]
+        public async Task<ActionResult<List<Birthday>>> GetBirthdaysOfToday()
+        {
+            using (var db = new BirthdayContext())
+            {
+                var birthdaysToday = db.Birthdays.Where(b => b.BirthDate.Day == DateTime.Today.Day && b.BirthDate.Month == DateTime.Today.Month).ToList();
+                return birthdaysToday;
+            }
+        }
+
+        [HttpGet("ThisWeek")]
+        public async Task<ActionResult<List<Birthday>>> GetBirthdaysOfThisWeek()
+        {
+            var today = DateTime.Today;
+            var nextWeek = DateTime.Today.AddDays(7);
+
+            using (var db = new BirthdayContext())
+            {
+                var todaysBirthdays = db.Birthdays.Where(b => b.BirthDate.AddYears(today.Year - b.BirthDate.Year) >= today && b.BirthDate.AddYears(today.Year - b.BirthDate.Year) <= nextWeek).ToList();
+                return todaysBirthdays;
             }
         }
     }
